9dfdfa7337da9e8dc2836de584751006201e4e16
Yao Jean-Claude <jeandev84@yahoo.com>
2021-05-09 18:52:40 +0300

    route collection

--- src/janklod/Component/Routing/RouteCollection.php
+++ src/janklod/Component/Routing/RouteCollection.php
@@ -27,6 +27,11 @@ class RouteCollection
      protected $resources = [];
 
 
+     /**
+      * @var array
+     */
+     protected $namedRoutes = [];
+
 
 
      /**
@@ -78,5 +83,70 @@ class RouteCollection
      public function addResource(Route $route)
      {
          $this->resources[] = $this->add($route);
+     }
+
+
+     /**
+      * @return array
+     */
+     public function getNamedRoutes(): array
+     {
+          foreach ($this->getRoutes() as $route)
+          {
+               if($name = $route->getName())
+               {
+                   $this->namedRoutes[$name] = $route;
+               }
+          }
+
+          return $this->namedRoutes;
+     }
+
+
+
+    /**
+     * @return array
+    */
+    public function getRoutesByMethod(): array
+    {
+        $routes = [];
+
+        foreach ($this->getRoutes() as $route)
+        {
+            /** @var Route $route */
+            $routes[$route->getMethodsToString()][] = $route;
+        }
+
+        return $routes;
+    }
+    
+    
+
+     /**
+      * @param string $name
+      * @param array $params
+      * @return false|string
+     */
+     public function generate(string $name, array $params = []): bool|string
+     {
+            if(! $this->has($name)) {
+                return false;
+            }
+
+            /** @var Route $route */
+            $route = $this->namedRoutes[$name];
+
+            return $route->convertParams($params);
+     }
+
+
+
+     /**
+      * @param $name
+      * @return bool
+     */
+     public function has($name): bool
+     {
+         return \array_key_exists($name, $this->getNamedRoutes());
      }
 }
